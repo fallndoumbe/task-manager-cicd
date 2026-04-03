88c8cee548ab6e36b6b64626a4a0f819ed0664ad
fallndoumbe <ndoumbefall163@gmail.com>
2026-04-03 15:02:04 +0000

    feat(tasks): création du controller

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function index() {
+    $tasks = Task::orderBy('created_at', 'desc')->get();
+    return view('tasks.index', compact('tasks'));
+}
+
+
+    public function create() {
+    return view('tasks.create');
+}
+
+      public function store(Request $request) {
+    $request->validate([
+        'title' => 'required|max:255',
+        'status' => 'required|in:todo,in_progress,done',
+        'priority' => 'required|in:low,medium,high',
+    ]);
+
+    Task::create($request->all());
+
+    return redirect()->route('tasks.index')->with('success', 'Tâche créée avec succés!');
+}
+}
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
