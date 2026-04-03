9082d852b9bf91aee3b5a90dc4520155533d256c
fallndoumbe <ndoumbefall163@gmail.com>
2026-04-03 15:34:06 +0000

    feat(tasks): creation du  model Task et controller

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -8,49 +8,58 @@ use Illuminate\Http\Request;
 class TaskController extends Controller
 {
     public function index() {
-    $tasks = Task::orderBy('created_at', 'desc')->get();
-    return view('tasks.index', compact('tasks'));
-}
-
+        $tasks = Task::orderBy('created_at', 'desc')->get();
+        return view('tasks.index', compact('tasks'));
+    }
 
     public function create() {
-    return view('tasks.create');
-}
+        return view('tasks.create');
+    }
 
-      public function store(Request $request) {
-    $request->validate([
-        'title' => 'required|max:255',
-        'status' => 'required|in:todo,in_progress,done',
-        'priority' => 'required|in:low,medium,high',
-    ]);
+    public function store(Request $request) {
+        $request->validate([
+            'title' => 'required|max:255',
+            'status' => 'required|in:todo,in_progress,done',
+            'priority' => 'required|in:low,medium,high',
+        ]);
 
-    Task::create($request->all());
+        Task::create($request->all());
 
-    return redirect()->route('tasks.index')->with('success', 'Tâche créée avec succés!');
-}
-}
+        return redirect()->route('tasks.index')
+            ->with('success', 'Tâche créée avec succès!');
+    }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
+
     public function edit(string $id)
     {
-        //
+        $task = Task::findOrFail($id);
+        return view('tasks.edit', compact('task'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+
+
     public function update(Request $request, string $id)
     {
-        //
+        $request->validate([
+            'title' => 'required|max:255',
+            'status' => 'required|in:todo,in_progress,done',
+            'priority' => 'required|in:low,medium,high',
+        ]);
+
+        $task = Task::findOrFail($id);
+        $task->update($request->all());
+
+        return redirect()->route('tasks.index')
+            ->with('success', 'Tâche mise à jour avec succès!');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+    
     public function destroy(string $id)
     {
-        //
+        $task = Task::findOrFail($id);
+        $task->delete();
+
+        return redirect()->route('tasks.index')
+            ->with('success', 'Tâche supprimée avec succès!');
     }
 }
