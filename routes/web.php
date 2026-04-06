5114c87e299637c67bdcdfa5323c596eb4cf224c
Lulu <bouange45@gmail.com>
2026-04-06 09:36:31 +0100

    feat(tasks): redirect root route to tasks index

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('tasks.index');
 });
-
 
 Route::resource('tasks', TaskController::class);
