b54a87dcc46b61758219d012afdf2fb5f6e39733
Lulu <bouange45@gmail.com>
2026-04-09 00:26:24 +0100

    fix: update test redirect to /api/tasks

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,6 +14,6 @@ class ExampleTest extends TestCase
     {
         $response = $this->get('/');
 
-        $response->assertRedirect('/tasks');
+        $response->assertRedirect('/api/tasks');
     }
 }
